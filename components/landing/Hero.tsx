c13235cb00e184361f6e6b5ce53fd2f8bbb9a728
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-08-09 19:44:06 +0100

    SETUP: added the globals.css, tailwind config and font and metadata to the layout for consitency and foundation

---
+++ components/landing/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <main>
+      <h1>This is the hero section</h1>
+    </main>
+  );
+}
---
+++ components/landing/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <header>
+      <p>This is the navbar</p>
+    </header>
+  );
+}
